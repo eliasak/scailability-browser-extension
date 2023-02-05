698f15461585cfe569f241366aa026928216c3bc
EliasAK <elias@webolize.com>
2023-02-05 00:48:59 -0500

    Initial checkin of rebrand

---
+++ src/utils.ts
@@ -0,0 +1,17 @@
+import Browser from 'webextension-polyfill'
+import { Theme } from './config'
+
+export function detectSystemColorScheme() {
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    return Theme.Dark
+  }
+  return Theme.Light
+}
+
+export function getExtensionVersion() {
+  return Browser.runtime.getManifest().version
+}
+
+export const API_HOST = 'https://scailability.com'
+//export const API_HOST = 'https://chatgpt4google.com'
+// const API_HOST = 'http://localhost:3000'
